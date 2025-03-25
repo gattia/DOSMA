24757881e54e6483f3cb0c0b9dcc058e8e9c6650
Anthony Gatt <anthony@neuralseg.com>
2025-03-25 11:23:39 -0700

    Update exclusion indices for axial processing in StanfordQDessBoneUNet2DSTAPLE class
    Update so that the patellar cartilage is predicted by 2 networks (axial and sagittal).

--- dosma/models/stanford_qdess_bone.py
+++ dosma/models/stanford_qdess_bone.py
@@ -277,7 +277,7 @@ class StanfordQDessBoneUNet2DSTAPLE():
     list_idx_not_include_STAPLE = [
         [], # what not to include for sagittal
         [1], # what not to include for coronal
-        [1, 2, 3, 4, 5, 6] # what not to include for axial
+        [2, 3, 4, 5, 6] # what not to include for axial
     ]
     # dict_tissues_combine_staple = {
     #     "pc": ["sag", "ax"],
